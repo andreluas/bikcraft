d9271d2968e2016194ca36e0d5cf17e283448eba
andreluas <andreluas.k@gmail.com>
2022-02-10 15:20:08 -0300

    functions

--- functions.php
+++ functions.php
@@ -15,5 +15,11 @@ remove_action('admin_print_styles', 'print_emoji_styles');
 // Habilitar Menus
 add_theme_support('menus');
 
+// Tamanhos de img
+function my_custom_sizes() {
+    add_image_size('large', 1400, 380, true);
+    add_image_size('medium', 768, 380, true);
+}
+add_action('after_setup_theme', 'my_custom_sizes');
 
 ?>
