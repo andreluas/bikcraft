67b5c9b36b74bdbbcab3ecbe68934e4b354fca0a
andreluas <andreluas.k@gmail.com>
2022-02-13 19:40:43 -0300

    update produtos

--- single-produtos.php
+++ single-produtos.php
@@ -3,18 +3,6 @@
 get_header();
 ?>
 	<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-		
-		<?php
-			$args = array (
-				'post_type' => 'produtos',
-				'order'   => 'ASC'
-			);
-			$the_query = new WP_Query ( $args );
-		?>
-
-		<?php if ( $the_query->have_posts() ) : while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
-			<h1><?php the_title(); ?></h1>
-		<?php endwhile; else: endif; ?>
 
 		<section class="container produto_item animar-interno">
 			<div class="grid-11">
@@ -28,30 +16,7 @@ get_header();
 			</div>
 		</section>
 
-		<section class="orcamento">
-			<div class="container">
-				<h2 class="subtitulo">Orçamento</h2>
-				<form action="enviar.php" method="post" name="form" class="formphp form grid-8">
-					<label for="nome">Nome</label>
-					<input id="nome" name="nome" type="text">
-					<label for="email">E-mail</label>
-					<input id="email" name="email" type="text">
-					<label for="telefone">Telefone</label>
-					<input id="telefone" name="telefone" type="text">
-
-					<label class="nao-aparece">Se você não é um robô, deixe em branco.</label>
-					<input type="text" class="nao-aparece" name="leaveblank">
-					<label class="nao-aparece">Se você não é um robô, não mude este campo.</label>
-					<input type="text" class="nao-aparece" name="dontchange" value="http://" >
-
-					<label for="mensagem">Especificações</label>
-					<textarea name="mensagem" id="mensagem"></textarea>
-
-					<button id="enviar" name="enviar" type="submit" class="btn">Enviar</button>
-				</form>
-				<?php include(TEMPLATEPATH . "/inc/produtos-orcamento.php"); ?>
-			</div>
-		</section>
+		<?php include(TEMPLATEPATH . "/inc/produtos-orcamento.php"); ?>
 	<?php endwhile; else: endif; ?>
 
 <?php get_footer(); ?>
